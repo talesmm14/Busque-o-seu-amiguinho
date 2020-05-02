1ba32f8747b1f7e90d9db42c904cc384bc386f56
talesmm14 <talesmelquiades@hotmail.com>
2020-05-02 05:59:31 -0300

    Update model Profile

--- core/models.py
+++ core/models.py
@@ -7,6 +7,12 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     github = models.URLField(verbose_name="GitHub", blank=True, default="")
 
+    bio = models.TextField(
+        "Sua Bio",
+        default="",
+        help_text="Descreva um pouco sobre você para as empresas poderem te conhecer melhor!",
+    )
+
     def __str__(self):
         return "{} {}".format(self.user.first_name, self.user.last_name)
 
