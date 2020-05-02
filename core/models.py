0530512460d0a83a42b9a3d9bea0fc3363689542
talesmm14 <talesmelquiades@hotmail.com>
2020-05-02 02:33:59 -0300

    model for user Profile

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Profile(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    github = models.URLField(verbose_name="GitHub", blank=True, default="")
 
 
 class StudyRoom(models.Model):
-    pass
+    users = models.ManyToManyField()
