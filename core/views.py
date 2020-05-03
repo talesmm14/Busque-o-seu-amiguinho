ccf764391b6a29236f6a7ac9d3dc8d6ea1616a1c
brunomedice <s9uk5KfpMdWjB5P>
2020-05-03 16:09:35 +1000

    Codigos de profiles refeitos em views.py e melhoras no profiles.html

--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from .models import Profile
+
 
 
 # Create your views here.
 from core.forms import RegisterForm
-
+from .models import Profile
 
 def page_home_view(request):
     return render(request, "pages/index.html")
@@ -27,9 +27,7 @@ def page_home_view(request):
 
 #create view_profiles
 def profiles_view(request):
-    user = Profile.user
-    github = Profile.github
-    bio = Profile.github
-    return render(request, "pages/profiles.html", {"user": user, "github": github, "bio": bio})
+    profiles = Profile.objects.all()
+    return render(request, "profiles.html", {"profiles": profiles})
 
 #create view_rooms
