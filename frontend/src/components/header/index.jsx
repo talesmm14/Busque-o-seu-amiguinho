89b711528f558c58bd81fd33b18d9fac0410a7f8
twdnl14 <twz3xc21@gmail.com>
2020-05-03 03:01:33 -0300

    1. Atualizando dependencias 2. Modificando a index(retirando algumas linhas e modificando a linguagem da pagina) 3. Construindo o header

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Navbar } from 'react-bootstrap';
+
 import './index.css'
+
 
 class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <h2>Bem vindo amiguinho!!!</h2>
+        <Navbar bg="light">
+          <Navbar.Brand>Busque o seu amiguinho!!!</Navbar.Brand>
+
+            <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <Navbar.Brand href="#">Entrar</Navbar.Brand>
+              <Navbar.Brand href="#">Registrar</Navbar.Brand>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+
       </div>
     )
   }
