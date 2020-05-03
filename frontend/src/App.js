9d37f76708b004a6f5124fa423e5e5b78f168eef
twdnl14 <twz3xc21@gmail.com>
2020-05-03 02:17:46 -0300

    Começando o front-end da aplicação em reactjs

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './components/header/index'
+
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/components/header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.css'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <h2>Bem vindo amiguinho!!!</h2>
+      </div>
+    )
+  }
+}
+
+export default Header;
