6a27df0d87908c2a072a63e014465991ae8d9d2e
talesmm14 <talesmelquiades@hotmail.com>
2020-05-03 02:01:15 -0300

    Update views

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from core.views import page_home_view
+from core.views import page_home_view, page_signup
 
 urlpatterns = [
     path('', page_home_view, name='page_home_view'),
+    path('signup/', page_signup, name='page_signup'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
+from core.forms import RegisterForm
+
 
 def page_home_view(request):
     return render(request, "pages/index.html")
+
+
+def page_signup(request):
+    context = {"form": RegisterForm(request.POST or None)}
+
+    if request.method == "POST" and context["form"].is_valid():
+        user = context["form"].save()
+        login(request, user)
+        return redirect("/")
+    return render(request, "signup.html", context)
