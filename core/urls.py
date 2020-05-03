a81097a86b69a64786937412841924e73e9437d7
brunomedice <s9uk5KfpMdWjB5P>
2020-05-03 15:05:29 +1000

    Adicionado pagina de Profiles, view e url

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from core.views import page_home_view
+from core.views import page_home_view, profiles_view
 
 urlpatterns = [
     path('', page_home_view, name='page_home_view'),
+    path('profiles/', profiles_view, name='profiles_view'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from .models import Profile
 
 # Create your views here.
 
 def page_home_view(request):
     return render(request, "pages/index.html")
+
+#create view_profiles
+def profiles_view(request):
+    user = Profile.user
+    github = Profile.github
+    bio = Profile.github
+    return render(request, "pages/profiles.html", {"user": user, "github": github, "bio": bio})
+
+#create view_rooms
+
